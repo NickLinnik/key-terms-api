f511bc515b7bdb8360565a4220081f968e37ac6b
Nick Linnik <nik.linnik.001@gmaul.com>
2022-07-13 19:05:36 +0300

    Define handlers

--- controller.py
+++ controller.py
@@ -5,14 +5,32 @@ from key_terms import KeyTerms
 app = Flask(__name__)
 
 
-@app.route('/key-terms-api/from-json')
-def hello_world():
+@app.route('/key-terms-api/from-json/single_text', methods=['POST'])
+def keywords_from_text():
     content_type = request.headers.get('Content-Type')
     if content_type == 'application/json':
-        json = request.json
-        print(json)
-        print(type(json))
-        return '42'
+        key_words_num = request.json['key_words_num']
+        texts = {'text': request.json['text']}
+
+        key_terms = KeyTerms(texts, key_words_num).keywords
+        print(key_terms)
+        return key_terms
+
+    else:
+        return 'Content-Type not supported!'
+
+
+@app.route('/key-terms-api/from-json/titled_texts', methods=['POST'])
+def keywords_from_labeled_texts():
+    content_type = request.headers.get('Content-Type')
+    if content_type == 'application/json':
+        key_words_num = request.json['key_words_num']
+        texts = request.json['texts']
+        print(texts)
+
+        key_terms = KeyTerms(texts, key_words_num).keywords
+        print(key_terms)
+        return key_terms
 
     else:
         return 'Content-Type not supported!'
